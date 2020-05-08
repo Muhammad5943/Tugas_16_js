66cb2a29b379499a251e471d752a9b1f5af4f973
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2020-05-08 21:37:50 +0700

    tugas16js
    mengumpulkan tugas 16 js

---
+++ tugas16.js
@@ -0,0 +1,21 @@
+function fstsentence() {
+    var sentence = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+
+    console.log(sentence.toLocaleLowerCase());
+    console.log(sentence.toLocaleUpperCase());
+
+    var secsentence = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+    console.log(sentence.toLocaleLowerCase());
+    console.log(sentence.toLocaleUpperCase());
+    
+}
+
+fstsentence()
+
+function secsentence() {
+    var kalimat = "Lorem Ipsum";
+    console.log(kalimat.charCodeAt(([5], 0-10)));
+    
+}
+
+secsentence()
